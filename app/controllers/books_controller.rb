3957a744af55b9098463ad6f2e4ed9b4b98c5879
EC2 Default User <yotchan32323@gmail.com>
2023-01-16 07:41:14 +0000

    [Add] 投稿機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,9 +4,21 @@ class BooksController < ApplicationController
     @book = Book.new
   end
 
+  def create
+    book = current_user.books.new(book_params)
+    book.save
+    redirect_to book_path(book)
+  end
+
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
