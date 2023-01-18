1c84a476d85cb40c54ca2fcfcd73b93e68453895
EC2 Default User <yotchan32323@gmail.com>
2023-01-18 08:50:33 +0000

    [Add] 投稿更新機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -18,8 +18,15 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
   def destroy
-    book = Book.find(params[:id]).destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
 
