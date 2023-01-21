84f8c58ba03b4a44cfe1e7ef042d83fb1d51ee90
EC2 Default User <yotchan32323@gmail.com>
2023-01-21 02:35:16 +0000

    [Add] book_commentsコントローラー

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   get "home/about" => "homes#about", as: "about"
   resources :books, only: [:create, :index, :show, :edit, :update, :destroy] do
     resource :favorites, only: [:create, :destroy]
+    resources :book_comments, only: [:create, :destroy]
   end
   resources :users, only: [:index, :show, :edit, :update]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
