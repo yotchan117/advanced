5e4809015ea40cb0d3958ceeef373fe703c7cd3e
EC2 Default User <yotchan32323@gmail.com>
2023-01-23 07:42:01 +0000

    [Add] relationshipsコントローラ

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,10 @@ Rails.application.routes.draw do
     resource :favorites, only: [:create, :destroy]
     resources :book_comments, only: [:create, :destroy]
   end
-  resources :users, only: [:index, :show, :edit, :update]
+  resources :users, only: [:index, :show, :edit, :update] do
+    resource :relationships, only: [:create, :destroy]
+    get "followings" => "relationships#followings"
+    get "followers" => "relationships#followers"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
