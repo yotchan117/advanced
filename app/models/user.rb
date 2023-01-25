3b91efd7c83b3226996230d221620df10325181e
EC2 Default User <yotchan32323@gmail.com>
2023-01-25 04:09:38 +0000

    [Add] モデルに検索用メソッドを記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -33,4 +33,16 @@ class User < ApplicationRecord
   def following?(user)
     followings.include?(user)
   end
+
+  def self.search_for(content, method)
+    if method == "perfect"
+      User.where(name: content)
+    elsif method == "forward"
+      User.where("name LIKE ?", content + "%")
+    elsif method == "backword"
+      User.where("name LIKE ?", "%" + content)
+    else
+      User.where("name LIKE ?", "%" + content + "%")
+    end
+  end
 end
