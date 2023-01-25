4a429ab6a042b47126e3baba419cc32142a1956d
EC2 Default User <yotchan32323@gmail.com>
2023-01-25 04:02:23 +0000

    [Add] 検索用ルーティング＆ビュー

--- config/routes.rb
+++ config/routes.rb
@@ -11,5 +11,6 @@ Rails.application.routes.draw do
     get "followings" => "relationships#followings"
     get "followers" => "relationships#followers"
   end
+  get "search" => "searches#search"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
