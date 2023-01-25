5c3607bb4493471d4532c5af73b79f68132c1baf
EC2 Default User <yotchan32323@gmail.com>
2023-01-25 03:47:14 +0000

    [Add] フォローボタン実装＆スペルミス修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,9 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
 
   has_many :relationships, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
-  has_many :reverse_of_relationships, class_name: "Relationship", foreign_kay: "followed_id", dependent: :destroy
+  has_many :reverse_of_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 
-  has_many :followings, through: :relationships, source: :follwed
+  has_many :followings, through: :relationships, source: :followed
   has_many :followers, through: :reverse_of_relationships, source: :follower
 
   validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
